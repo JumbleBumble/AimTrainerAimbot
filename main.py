666f111559cc84fc456b1d95f390206735d08aed
JumbleBumble <113152254+JumbleBumble@users.noreply.github.com>
2023-11-05 16:21:01 -0500

    Offset Update
     - Added X Offset and Y Offset options
     - Added region to library and import PositionDialog from that
     - Updated Stylesheets and added background images

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pyautogui
 import threading
 from library.AutoAimTrainer import AimTrainer as AutoAimTrainer
+from library.region import PositionDialog
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -10,30 +11,11 @@ from PyQt5.QtWidgets import (
     QGridLayout,
     QWidget,
     QColorDialog,
-    QDialog,
+    QSpinBox,
+    QHBoxLayout 
 )
 from PyQt5.QtCore import Qt
 
-
-class PositionDialog(QDialog):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("Region")
-        self.setGeometry(200, 200, 300, 200)
-        self.setStyleSheet("background-color: black;")
-        self.setWindowOpacity(0.4)
-
-    def get_region(self):
-        window_pos = self.pos()
-        window_width = self.width()
-        window_height = self.height()
-
-        top_left_x = window_pos.x()
-        top_left_y = window_pos.y()
-        bottom_right_x = top_left_x + window_width
-        bottom_right_y = top_left_y + window_height
-        return [top_left_x, top_left_y], [bottom_right_x, bottom_right_y]
 
 
 class MainWindow(QMainWindow):
@@ -43,6 +25,7 @@ class MainWindow(QMainWindow):
         self.thread,self.color = None, None
         self.pos_dialog = PositionDialog()
         self.top_left, self.bottom_right = [0, 0], [0, 0]
+        self.x_offset, self.y_offset = 0, 0
         self.initUI()
 
     #Opens color prompt and collects the values
@@ -75,6 +58,8 @@ class MainWindow(QMainWindow):
             while self.aimbot_active:
                 AutoAimTrainer.find_color(
                     self.color,
+                    x_offset = self.x_offset,
+                    y_offset = self.y_offset,
                     x_min=self.top_left[0],
                     y_min=self.top_left[1],
                     x_max=self.bottom_right[0],
@@ -105,23 +90,50 @@ class MainWindow(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle("Aim Trainer Aimbot")
-        self.setFixedSize(350, 100)
+        self.setFixedSize(350, 150)
         self.setStyleSheet(
             """
-        QMainWindow{
-            background-color: #6c6c6c;
+        QMainWindow {
+            background-image: url(images/main-background.png);
         }
-        QLabel{
+
+        QLabel {
             border-radius: 2px;
-            background-color:#474747;
-            color: white;
-            border: 1px solid white;
+            background-color: #343a40; 
+            color: #ffffff; 
+            max-height: 20px;
         }
-        QPushButton{
+
+        QPushButton {
             border-radius: 2px;
-            border: 1px solid black;
-            padding: 2px;
-            background-color: #e6e6e6;
+            border: 1px solid #000000;
+            background-color: #007bff; 
+            color: #ffffff;
+            padding: 4px;
+        }
+
+        QPushButton:hover {
+            background-color: #0056aa; 
+        }
+
+        QSpinBox {
+            border: 1px solid #000000; 
+            background-color: #ffffff; 
+            color: #000000; 
+        }
+
+        QSpinBox::up-button {
+            background-image: url(images/ArrowUp.png);
+         
+      
+        }
+
+        QSpinBox::down-button {
+            background-image: url(images/ArrowDown.png);
+            background-repeat: none;
+            margin-left: 1px;
+            margin-bottom: 1px;
+
         }
         """
         )
@@ -145,7 +157,7 @@ class MainWindow(QMainWindow):
         self.region_label.setAlignment(Qt.AlignCenter)
         self.color_label.setAlignment(Qt.AlignCenter)
 
-        # Signal setup
+        # Button signal setup
         [
             signal.connect(slot)
             for signal, slot in [
@@ -155,6 +167,32 @@ class MainWindow(QMainWindow):
             ]
         ]
 
+        # Inner layout setup
+        self.inner_layout = QHBoxLayout()
+        self.x_label,self.x_offset_input = QLabel("X Offset:"),QSpinBox()
+        self.Y_label,self.y_offset_input = QLabel("Y Offset:"),QSpinBox()
+        self.Y_label.setAlignment(Qt.AlignCenter),self.x_label.setAlignment(Qt.AlignCenter)
+        self.x_offset_input.setMinimum(-2147483648),self.x_offset_input.setMaximum(2147483647)
+        self.y_offset_input.setMinimum(-2147483648),self.y_offset_input.setMaximum(2147483647)
+
+        [
+            self.inner_layout.addWidget(widget)
+            for widget in [
+                (self.x_label),
+                (self.x_offset_input),
+                (self.Y_label),
+                (self.y_offset_input)
+            ]
+        ]
+
+        # Handles spinbox changes
+        def offset(typ=None):
+            print('kew')
+            if typ == 'y': self.y_offset = self.y_offset_input.value()
+            else: self.x_offset = self.x_offset_input.value()
+
+        self.x_offset_input.valueChanged.connect(offset),self.y_offset_input.valueChanged.connect(lambda: offset('y'))
+
         # layout setup
         [
             layout.addWidget(*widget)
@@ -163,9 +201,10 @@ class MainWindow(QMainWindow):
                 (self.color_label, 0, 0, 1, 1),
                 (self.color_button, 2, 0, 1, 1),
                 (self.region_button, 2, 1, 1, 1),
-                (self.aimbot_button, 3, 0, 1, 2),
+                (self.aimbot_button, 5, 0, 1, 2),
             ]
         ]
+        layout.addLayout(self.inner_layout, 4, 0, 1, 2)
         central_widget.setLayout(layout)
 
 
